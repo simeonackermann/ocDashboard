77b6412b8bfb45de3d8b893aee7e5b54996ebea7
Florian Steffens <flost@live.no>
2013-09-11 10:33:28 +0200

    missing translation

--- l10n/de.php
+++ l10n/de.php
@@ -51,6 +51,7 @@ $TRANSLATIONS = array(
 "Supports only imap." => "Unterstützt leider nur imap Postfächer.",
 "Max age of news to show (hours)" => "Maximales Alter der angezeigten Nachrichten (in Stunden)",
 "Missing required app." => "Es wurden nicht alle benötigten Apps gefunden.",
+"Missing settings." => "Es fehlen Einstellungen.",
 "The city code is not valid." => "Der Code für die Stadt ist nicht korrekt.",
 "No news found." => "Es wurden keine Neuigkeiten gefunden.",
 "You can get the code for your city here: <a href=\"http://weather.yahoo.com\" target=\"_blank\">http://weather.yahoo.com/</a><br />Look for your city and open the weather information for it.<br />Look at the URL. It should end like /city-xxxxxx/.<br />Type in here the City Code \"xxxxxx\"." => "Den Code für deine Stadt bekommst du hier: <a href=\"http://weather.yahoo.com\" target=\"_blank\">http://weather.yahoo.com/</a><br />Suche deine Wunschstadt und öffne die Wetterinformationen.<br />Sieh dir die URL an. Am Ende steht folgendes /city-xxxxxx/.<br />Gib hier als city code die Ziffern \"xxxxxx\" an.",
